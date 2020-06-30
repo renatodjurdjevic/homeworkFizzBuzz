158069cd07f318bb8c3304eeafecfe968acebf46
renatodjurdjevic <renato.durdevic23@gmail.com>
2020-06-30 19:31:53 +0200

    initial commit

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+# fizz buzz game
+
+print("- Fizz Buzz Game -")
+
+end = int(input("Please pick a number from 1 to 100: "))
+end = int(end)
+
+for x in range(1, end+1):
+
+    if x % 3 == 0 and x % 5 == 0:
+        print("fizzbuzz")
+    elif x % 3 == 0:
+        print("fizz")
+    elif x % 5 == 0:
+        print("buzz")
+    else:
+        print(x)
